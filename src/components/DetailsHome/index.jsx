2a6161aaef5b92758da5c4ca7f589bd3a76fa9fc
Fredy Alexander España Garcia <faespanag@uqvirtual.edu.co>
2023-09-18 17:04:55 -0500

    Modificacion y agrega header nuevo

--- src/components/DetailsHome/index.jsx
+++ src/components/DetailsHome/index.jsx
@@ -1,45 +1,48 @@
-import React from 'react';
-import ImgX from '../../assets/imgHome/X1.jpg'
+import React from "react";
+import ImgX from "../../assets/imgHome/X1.jpg";
+import HeaderProducts from "../HeaderProducts/HeaderProduct";
 
 const DetailsHome = () => {
-  return (
-    <div className="bg-pink-800 text-white py-12">
-      <div className="container mx-auto text-center">
-        <h1 className="text-4xl font-semibold mb-6 animate-fade-in-up">
-          ¡Bienvenido Desiderium nuestra Tienda Erótica!
-        </h1>
-        <p className="text-lg mb-8 animate-fade-in-up-delay">
-          Descubre una amplia selección de productos sensuales para añadir un
-          toque de pasión a tu vida.
-        </p>
-        <button className="bg-pink-500 hover:bg-pink-400 text-white py-2 px-6 rounded text-lg font-semibold animate-bounce">
-          Explora Nuestros Productos
-        </button>
-        <div className="mt-12 animate-fade-in-up-delay">
-          <img
-            src="https://cdn.pixabay.com/photo/2020/06/16/05/20/nude-5304236_1280.jpg" // Reemplaza con la URL de tu imagen
-            alt="Productos Eróticos"
-            className="mx-auto max-w-full rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
-          />
-        </div>
-        <div className="mt-12">
-          <h2 className="text-3xl font-semibold mb-6 animate-fade-in-up">
-            Nuestros Destacados
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="bg-pink-700 p-4 rounded animate-fade-in-up-delay transform hover:scale-105 transition-transform duration-300">
-              <img
-                src="https://cdn.pixabay.com/photo/2020/06/16/05/20/nude-5304236_1280.jpg" // Reemplaza con la URL de la imagen del producto
-                alt="Producto Destacado"
-                className="mx-auto max-w-full rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 "
-              />
-              <h3 className="text-lg font-semibold mt-2">Nombre del Producto</h3>
-              <p className="text-gray-300">Descripción del producto...</p>
+  return (  
+      <div className="bg-pink-800 text-white py-12 mt-80 sm:mt-32 md:mt-32 lg:mt-36">
+        <div className="container mx-auto text-center ">
+          <h1 className="text-4xl font-semibold mb-6 animate-fade-in-up">
+            ¡Bienvenido Desiderium nuestra Tienda Erótica!
+          </h1>
+          <p className="text-lg mb-8 animate-fade-in-up-delay">
+            Descubre una amplia selección de productos sensuales para añadir un
+            toque de pasión a tu vida.
+          </p>
+          <button className="bg-pink-500 hover:bg-pink-400 text-white py-2 px-6 rounded text-lg font-semibold animate-bounce z-0" >
+            Explora Nuestros Productos
+          </button>
+          <div className="mt-12 animate-fade-in-up-delay">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/06/16/05/20/nude-5304236_1280.jpg" // Reemplaza con la URL de tu imagen
+              alt="Productos Eróticos"
+              className="mx-auto max-w-full rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
+            />
+          </div>
+          <div className="mt-12">
+            <h2 className="text-3xl font-semibold mb-6 animate-fade-in-up">
+              Nuestros Destacados
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <div className="bg-pink-700 p-4 rounded animate-fade-in-up-delay transform hover:scale-105 transition-transform duration-300">
+                <img
+                  src="https://cdn.pixabay.com/photo/2020/06/16/05/20/nude-5304236_1280.jpg" // Reemplaza con la URL de la imagen del producto
+                  alt="Producto Destacado"
+                  className="mx-auto max-w-full rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 "
+                />
+                <h3 className="text-lg font-semibold mt-2">
+                  Nombre del Producto
+                </h3>
+                <p className="text-gray-300">Descripción del producto...</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,69 +8,78 @@ import {
   faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import HeaderProducts from "../HeaderProducts/HeaderProduct";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   const numPedidos = 5; // Cambia esto con la cantidad real de pedidos
   return (
-    <div className="shadow-md w-full fixed top-0 left-0 z-10">
-      <div className="md:flex items-center justify-between bg-black text-white py-4 md:px-10 px-7">
-        <div
-          className="font-bold text-2xl cursor-pointer flex flex-col items-center font-[Poppins] 
+    <>
+      <div className="shadow-md w-full fixed top-0 left-0 z-20">
+        <div className="md:flex items-center justify-between bg-black text-white py-4 md:px-10 px-7">
+          <div
+            className="font-bold text-2xl cursor-pointer flex flex-col items-center font-[Poppins] 
       text-pink-500"
-        >
-          <span>{STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM}</span>
-          <span>
-            {STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM_1}&nbsp;&nbsp;
-            {STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM_2}
-          </span>
-        </div>
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-        >
-          <FontAwesomeIcon
-            icon={open ? faTimes : faBars}
-            className="text-white hover:text-gray-400 cursor-pointer"
-          />
-        </div>
-        <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 " : "top-[-490px]"
-          }`}
-        >
-          {TITLE_MENU_NAVBAR.map((link) => (
-            <li key={link.name} className="md:ml-8 text-sm md:my-0 my-7">
-              <Link
-                href={link.link}
-                className="text-white hover:text-gray-400 duration-500"
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-          <Link href="/cart">
-            <div className="text-3xl relative  sm:ml-6 md:ml-6 lg:ml-6">
-              <FontAwesomeIcon
-                icon={faShoppingCart}
-                className="text-white hover:text-gray-400 cursor-pointer"
-              />
-              {numPedidos > 0 && (
-                <span className="relative -top-8 -right-3 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-sm transform translate-y-[-50%] translate-x-1/2 transition-all duration-300">
-                  {numPedidos}
-                </span>
-              )}
-            </div>
-          </Link>
-          <button
-            className="bg-pink-500 text-white font-[Poppins] text-sm py-2 px-6 rounded md:ml-8 hover:bg-pink-400 
-    duration-500"
           >
-            {STRINGS.NAVBAR_BUTTON_LOGIN}
-          </button>
-        </ul>
+            <span>{STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM}</span>
+            <span>
+              {STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM_1}&nbsp;&nbsp;
+              {STRINGS.NAVBAR_BUTTON_TITLE_DESIDERIUM_2}
+            </span>
+          </div>
+          <div
+            onClick={() => setOpen(!open)}
+            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          >
+            <FontAwesomeIcon
+              icon={open ? faTimes : faBars}
+              className="text-white hover:text-gray-400 cursor-pointer"
+            />
+          </div>
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+              open ? "top-20 " : "top-[-490px]"
+            }`}
+          >
+            {TITLE_MENU_NAVBAR.map((link) => (
+              <li key={link.name} className="md:ml-8 text-sm md:my-0 my-7">
+                <Link
+                  href={link.link}
+                  className="text-white hover:text-gray-400 duration-500"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+            <Link href="/cart">
+              <div className="text-3xl relative  sm:ml-6 md:ml-6 lg:ml-6">
+                <FontAwesomeIcon
+                  icon={faShoppingCart}
+                  className="text-white hover:text-gray-400 cursor-pointer"
+                />
+                {numPedidos > 0 && (
+                  <span className="relative -top-8 -right-3 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-sm transform translate-y-[-50%] translate-x-1/2 transition-all duration-300">
+                    {numPedidos}
+                  </span>
+                )}
+              </div>
+            </Link>
+            <button
+              className="bg-pink-500 text-white font-[Poppins] text-sm py-2 px-6 rounded md:ml-8 hover:bg-pink-400 
+    duration-500"
+            >
+              {STRINGS.NAVBAR_BUTTON_LOGIN}
+            </button>
+          </ul>
+        </div>
+       
       </div>
-    </div>
+      <div className="pt-32 bg-white shadow-md w-full fixed top-0 left-0 pb-10 z-10">
+          <div className=" block  sm:flex  md:flex lg:flex justify-center items-center">
+            <HeaderProducts />
+          </div>
+        </div>
+    </>
   );
 };
 

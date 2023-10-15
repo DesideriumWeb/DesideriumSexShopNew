10b3ccec91c28897121d44935565826164d5c02c
Fredy Alexander España Garcia <faespanag@uqvirtual.edu.co>
2023-10-15 16:17:14 -0500

    Modificaicon en carrito de compras

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,54 +1,72 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const CartPage = () => {
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: "Producto 1",
-      price: 10,
-      quantity: 2,
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2014/06/28/16/04/sex-toys-378988_1280.jpg",
-      description: "Descripción del producto 1.",
-    },
-    {
-      id: 2,
-      name: "Producto 2",
-      price: 20,
-      quantity: 1,
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2014/06/28/16/04/sex-toys-378988_1280.jpg",
-      description: "Descripción del producto 2.",
-    },
-  ]);
+  const [cartItems, setCartItems] = useState([]);
+
+  // Función para obtener el carrito desde el localStorage
+  const getCart = () => {
+    const cart = localStorage.getItem("cart");
+    return cart ? JSON.parse(cart) : [];
+  };
+
+  useEffect(() => {
+    // Cargar el carrito almacenado en el localStorage cuando el componente se monta
+    const initialCart = getCart();
+    setCartItems(initialCart);
+  }, []); // El segundo argumento [] asegura que el efecto se ejecute solo una vez al montar el componente
 
   const calculateTotal = () => {
     return cartItems.reduce(
-      (total, item) => total + item.price * item.quantity,
+      (total, item) => total + item.precio * item.cantidad,
       0
     );
   };
 
   const increaseQuantity = (itemId) => {
     const updatedCart = cartItems.map((item) =>
-      item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      item.id === itemId ? { ...item, cantidad: item.cantidad + 1 } : item
     );
     setCartItems(updatedCart);
+    // Actualizar el carrito en el localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
   const decreaseQuantity = (itemId) => {
     const updatedCart = cartItems.map((item) =>
-      item.id === itemId && item.quantity > 1
-        ? { ...item, quantity: item.quantity - 1 }
+      item.id === itemId && item.cantidad > 1
+        ? { ...item, cantidad: item.cantidad - 1 }
         : item
     );
     setCartItems(updatedCart);
+    // Actualizar el carrito en el localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
   const removeItem = (itemId) => {
+    // Filtrar el producto que deseas eliminar
     const updatedCart = cartItems.filter((item) => item.id !== itemId);
     setCartItems(updatedCart);
+    // Actualizar el carrito en el localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+  const handleBrasierTallaChange = (itemId, selectedTalla) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId ? { ...item, tallaBrasier: selectedTalla } : item
+    );
+    setCartItems(updatedCart);
+    // Actualizar el carrito en el localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+  const handlePantyTallaChange = (itemId, selectedTalla) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId ? { ...item, tallaPanty: selectedTalla } : item
+    );
+    setCartItems(updatedCart);
+    // Actualizar el carrito en el localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
   return (
@@ -63,52 +81,101 @@ const CartPage = () => {
             className="bg-white rounded-md p-4 mb-4 flex text-center sm:text-left md:text-left lg:text-left flex-col sm:flex-row md:flex-row items-center shadow-md"
           >
             <img
-              src={item.imageUrl}
+              src={item.imagePath}
               alt={item.name}
-              className="w-20 h-20 object-cover rounded-full mr-4"
+              className="w-44 h-44 object-cover rounded-full mr-4 hover:scale-105 transition-transform duration-300"
             />
             <div className="flex-grow font-semibold">
               <h2 className="text-lg ">{item.name}</h2>
-              <p className="text-black">Descripción: {item.description}</p>
-              <p className="text-black">Precio: ${item.price} Und</p>
-              <div className="flex items-center justify-center sm:justify-start md:justify-start mt-2">
+              <p className="text-black font-sans">
+                Descripción:{" "}
+                <span className="font-normal">{item.description}</span>
+              </p>
+              <p className="text-black font-sans">
+                Precio: ${item.precio.toLocaleString()} Und
+              </p>
+
+              {/* Dropdown de selección de talla de Brasier */}
+              <label
+                htmlFor={`talla-brasier-${item.id}`}
+                className="block text-black mt-2"
+              >
+                Talla de Brasier:
+              </label>
+              <select
+                id={`talla-brasier-${item.id}`}
+                value={item.tallaBrasier}
+                onChange={(e) =>
+                  handleBrasierTallaChange(item.id, e.target.value)
+                }
+                className="w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-[#D50CD5] text-black mt-1"
+              >
+                <option value="">Seleccionar talla de Brasier</option>
+                <option value="32">32</option>
+                <option value="34">34</option>
+                <option value="36">36</option>
+                <option value="38">38</option>
+              </select>
+
+              {/* Dropdown de selección de talla de Panty */}
+              <label
+                htmlFor={`talla-panty-${item.id}`}
+                className="block text-black mt-2 font-sans"
+              >
+                Talla de Panty:
+              </label>
+              <select
+                id={`talla-panty-${item.id}`}
+                value={item.tallaPanty}
+                onChange={(e) =>
+                  handlePantyTallaChange(item.id, e.target.value)
+                }
+                className="w-full  p-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-[#D50CD5] text-black mt-1"
+              >
+                <option value="" className="font-sans">Seleccionar talla de Panty</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+              </select>
+
+              <div className="flex flex-row sm:flex-row md:flex-row">
                 <button
                   onClick={() => decreaseQuantity(item.id)}
-                  className="text-[#D50CD5] font-semibold mr-2"
+                  className="text-[#D50CD5] font-bold mr-2 text-2xl"
                 >
                   -
                 </button>
-                <p className="text-lg">{item.quantity}</p>
+                <p className="text-lg">{item.cantidad}</p>
                 <button
                   onClick={() => increaseQuantity(item.id)}
-                  className="text-[#D50CD5] font-semibold ml-2"
+                  className="text-[#D50CD5] font-bold ml-2 text-2xl"
                 >
                   +
                 </button>
               </div>
-            </div>
-            <div className="flex flex-col sm:flex-row md:flex-row">
               <p className="text-lg font-semibold">
-                ${item.price * item.quantity}
+                Total por producto ${(item.precio * item.cantidad).toLocaleString()}
               </p>
-              <button
-                onClick={() => removeItem(item.id)}
-                className="bg-[#D50CD5] text-white py-2 px-4 rounded mt-1 ml-2 block mx-auto hover:bg-[#9806A9] transition-colors duration-300 md:hidden" // Ocultar en dispositivos pequeños (md:hidden)
-              >
-                Eliminar
-              </button>
-              <button
-                onClick={() => removeItem(item.id)}
-                className="bg-[#D50CD5] text-white py-2 px-4 rounded mt-1 ml-2 hidden md:block" // Mostrar en dispositivos medianos y mayores (md:block)
-              >
-                Eliminar
-              </button>
             </div>
+            <button
+              onClick={() => removeItem(item.id)}
+              className="bg-[#D50CD5] text-white py-2 px-4 rounded mt-1 ml-2 block mx-auto hover:bg-[#9806A9] transition-colors duration-300 md:hidden"
+            >
+              Eliminar
+            </button>
+            <button
+              onClick={() => removeItem(item.id)}
+              className="bg-[#D50CD5] text-white py-2 px-4 rounded mt-1 ml-2 hidden md:block"
+            >
+              Eliminar
+            </button>
           </div>
         ))}
         <div className="border-t pt-4">
           <p className="text-xl font-semibold text-center text-[#D50CD5]">
-            Total: ${calculateTotal()}
+            Total: ${calculateTotal().toLocaleString()}
           </p>
           <button className="bg-[#D50CD5] text-white py-2 px-4 rounded mt-4 block mx-auto hover:bg-[#9806A9] transition-colors duration-300">
             Comprar

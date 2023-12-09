6159d590862b22a960aa16ec82eef9a61712131d
Fredy Alexander España Garcia <faespanag@uqvirtual.edu.co>
2023-12-09 12:28:52 -0500

    cambio de icono

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "tailwindcss/tailwind.css";
@@ -7,7 +6,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import WhatsapIcon from "@/components/WhatsapIcon";
 import AuthContextProvider from "@/contexts/authContext";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +22,27 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Tiro+Devanagari+Hindi&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/icon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/icon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/icon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/public/icon/site.webmanifest" />
+        <link rel="mask-icon" href="/public/icon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>
         <main className="font-global">
@@ -33,7 +52,7 @@ export default function RootLayout({ children }) {
             <AuthContextProvider>{children}</AuthContextProvider>
           </div>
           <Footer />
-         <WhatsapIcon/>
+          <WhatsapIcon />
         </main>
       </body>
     </html>
